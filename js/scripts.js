02f9737721c7c0b86507b762938a92d7d3ed4922
Joseph Karasek and Michael Dada <mail.michaeldada@gmail.com>
2016-01-12 11:03:01 -0800

    add removeClass to js so colors can switch back and forth

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
   $("button#redBtn").click(function() {
+    $("body").removeClass();
     $("body").addClass("redBackground");
   });
   $("button#blueBtn").click(function() {
+    $("body").removeClass();
     $("body").addClass("blueBackground");
   });
   $("button#greenBtn").click(function() {
+    $("body").removeClass();
     $("body").addClass("greenBackground");
   });
 
